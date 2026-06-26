5e8fc2dc8c40f2691c038d6e6428cd7e2688ccdb
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-06-26 09:59:36 +0530

    feat: rendering the status bar correctly, and some convinience methods added.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-use crate::row::Row;
+use crate::{
+    buffer::TextBuffer,
+    renderer::Renderer,
+    status_bar::StatusBar,
+    terminal::{Terminal, TerminalGuard},
+};
+
+use anyhow::Result;
+use std::{
+    io::{self, Write},
+    time::Duration,
+};
 
 mod buffer;
 mod color;
@@ -8,16 +19,27 @@ mod history;
 mod lang;
 mod renderer;
 mod row;
+mod status_bar;
 mod terminal;
 
 /* TODO 1. document the code and for simple functions just introduce what
 the function does.
 */
 
-fn main() {
-    let row = Row::new("char with \t tab. \twith\t ¬˚˙¬˚∆ƒß");
-    match row {
-        Err(err) => println!("erorr: {}", err),
-        Ok(result) => println!("{:?}", result),
-    }
+fn main() -> Result<()> {
+    let _guard = TerminalGuard::enter()?;
+    let size = Terminal::size()?;
+    let mut renderer = Renderer::new(size, io::stdout())?;
+    renderer.set_info_msg("Test Message: The engine is working");
+    let mut buf = TextBuffer::empty();
+    buf.set_file("test.rs");
+    let status = StatusBar::from_buffer(&buf, (1, 1));
+
+    let mut stdout = io::stdout();
+
+    renderer.draw_status_bar(&mut stdout, &status)?;
+    renderer.draw_message_bar(&mut stdout, renderer.message.as_ref().unwrap())?;
+    stdout.flush()?;
+    std::thread::sleep(Duration::from_secs(10));
+    Ok(())
 }
--- src/renderer.rs
+++ src/renderer.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 
 use std::{io::Write, time::SystemTime};
 
@@ -5,11 +6,12 @@ use anyhow::{Context, Result, bail};
 use crossterm::{
     cursor::{Hide, MoveTo, Show},
     queue,
-    style::Print,
+    style::{Attribute, Color, Print, ResetColor, SetAttribute, SetBackgroundColor},
     terminal::{Clear, ClearType},
 };
+use unicode_width::UnicodeWidthChar;
 
-use crate::terminal::Size;
+use crate::{status_bar::StatusBar, terminal::Size};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum StatusMessageKind {
@@ -26,7 +28,11 @@ pub struct StatusMessage {
 
 impl StatusMessage {
     pub fn new<S: Into<String>>(msg: S, kind: StatusMessageKind) -> Self {
-        self.
+        Self {
+            text: msg.into(),
+            timestamp: SystemTime::now(),
+            kind,
+        }
     }
 }
 
@@ -39,8 +45,20 @@ pub enum DrawMessage {
 }
 
 impl DrawMessage {
-    pub fn fold(self, rhs: Self) -> Self {
-        todo!()
+    pub fn merge(self, rhs: Self) -> Self {
+        use DrawMessage::*;
+        match (self, rhs) {
+            (lhs, DoNothing) => lhs,
+            (DoNothing, rhs) => rhs,
+            (Update, Update) => Update,
+            (Open, Update) => Open,
+            (Update, Close) => Close,
+            (Close, Open) => Update,
+            (Open, Close) => DoNothing,
+            (lhs, rhs) => {
+                unreachable!("invalid DrawMessage transition: {:?} then {:?}", lhs, rhs)
+            }
+        }
     }
 }
 
@@ -62,7 +80,7 @@ pub struct Renderer<W: Write> {
     row_idx: usize,
 
     /// The active transient status message, if any.
-    msg: Option<StatusMessage>,
+    pub message: Option<StatusMessage>,
 
     /// The current operational state of the message bar.
     draw_message: DrawMessage,
@@ -97,6 +115,7 @@ impl<W: Write> Renderer<W> {
         output
             .flush()
             .context("Failed to flush intial cursor hide sequence")?;
+
         Ok(Self {
             // The text grid is strictly 2 rows shorter than the window; to reserve
             // permanent space for the Status Bar (row N-1) and Message Bar (row N)
@@ -104,9 +123,9 @@ impl<W: Write> Renderer<W> {
             num_cols: width,
             output,
             row_idx: 0,
-            msg: todo!(),
-            draw_message: todo!(),
-            redraw_idx: todo!(),
+            message: None,
+            draw_message: DrawMessage::DoNothing,
+            redraw_idx: Some(0),
             cursor_moved: true,
             rowoff: 0,
             coloff: 0,
@@ -154,5 +173,132 @@ impl<W: Write> Renderer<W> {
             Show
         )?;
         self.write_sync(&screen_buf)
+    }
+}
+
+impl<W: Write> Renderer<W> {
+    fn set_msg(&mut self, status_msg: Option<StatusMessage>) {
+        let rhs = match (&self.message, &status_msg) {
+            (Some(prev), Some(next)) if prev.text == next.text => DrawMessage::DoNothing,
+            (Some(_), Some(_)) => DrawMessage::Update,
+            (None, Some(_)) => DrawMessage::Open,
+            (Some(_), None) => DrawMessage::Close,
+            (None, None) => DrawMessage::DoNothing,
+        };
+        self.draw_message = self.draw_message.merge(rhs);
+        self.message = status_msg;
+    }
+
+    pub fn set_info_msg<S: Into<String>>(&mut self, msg: S) {
+        use StatusMessageKind::*;
+        self.set_msg(Some(StatusMessage::new(msg, Info)));
+    }
+
+    pub fn set_error_msg<S: Into<String>>(&mut self, msg: S) {
+        use StatusMessageKind::*;
+        self.set_msg(Some(StatusMessage::new(msg, Error)));
+    }
+
+    pub fn remove_msg(&mut self) {
+        self.set_msg(None);
+    }
+
+    pub fn set_redraw_idx(&mut self, index: usize) {
+        if let Some(ridx) = self.redraw_idx
+            && ridx < index
+        {
+            return;
+        }
+        self.redraw_idx = Some(index);
+    }
+
+    pub fn rows(&self) -> usize {
+        if self.message.is_some() {
+            self.num_rows
+        } else {
+            self.num_rows + 1
+        }
+    }
+
+    pub fn cols(&self) -> usize {
+        self.num_cols
+    }
+
+    pub fn message_text(&self) -> &str {
+        self.message.as_ref().map(|m| m.text.as_str()).unwrap_or("")
+    }
+}
+
+impl<W: Write> Renderer<W> {
+    fn update_message_bar(&mut self) -> Result<()> {
+        if let Some(msg) = &self.message
+            && let Ok(elapsed) = msg.timestamp.elapsed()
+            && elapsed.as_secs() > 5
+        {
+            self.remove_msg();
+        }
+        if self.draw_message == DrawMessage::Close {
+            // Closing the message bar reveals one more line of text at the bottom
+            // of the grid.
+            self.set_redraw_idx(self.num_rows);
+        }
+        Ok(())
+    }
+
+    fn trim_visual(text: &str, max_width: usize) -> (&str, usize) {
+        let mut curr_width = 0;
+        for (byte_idx, ch) in text.char_indices() {
+            let ch_width = ch.width_cjk().unwrap_or(1);
+            if curr_width + ch_width > max_width {
+                return (&text[..byte_idx], curr_width);
+            }
+            curr_width += ch_width;
+        }
+        (text, curr_width)
+    }
+
+    pub fn draw_message_bar<B: Write>(&self, mut buf: B, msg: &StatusMessage) -> Result<()> {
+        let (trimmed, _) = Self::trim_visual(&msg.text, self.num_cols);
+
+        queue!(buf, MoveTo(0, self.num_rows as u16 + 1))?;
+        if msg.kind == StatusMessageKind::Error {
+            queue!(buf, SetBackgroundColor(Color::Red))?;
+        }
+        queue!(
+            buf,
+            Print(trimmed),
+            ResetColor,
+            Clear(ClearType::UntilNewLine)
+        )?;
+        Ok(())
+    }
+
+    pub fn draw_status_bar<B: Write>(&self, buf: &mut B, status_bar: &StatusBar) -> Result<()> {
+        let right = status_bar.right();
+        let left = status_bar.left();
+
+        let (left_str, llen) = Self::trim_visual(&left, self.num_cols);
+
+        queue!(
+            buf,
+            MoveTo(0, self.num_rows as u16),
+            SetAttribute(Attribute::Reverse),
+            Print(left_str)
+        )?;
+        let rem_cols = self.num_cols.saturating_sub(llen);
+
+        let (right_str, rlen) = Self::trim_visual(&right, rem_cols);
+        let padding = rem_cols.saturating_sub(rlen);
+
+        if padding > 0 {
+            queue!(buf, Print(" ".repeat(padding)))?;
+        }
+        queue!(
+            buf,
+            Print(right_str),
+            SetAttribute(Attribute::Reset),
+            Clear(ClearType::UntilNewLine),
+        )?;
+        Ok(())
     }
 }
