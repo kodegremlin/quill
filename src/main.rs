d1cd2ad77f4f3142d30ab826ee0f5c02afff85c0
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-06-29 05:45:58 +0530

    dep: logging

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,9 @@ use crate::{
 
 use anyhow::Result;
 use clap::Parser;
+use env_logger::Target;
 use std::{
+    fs::{self, File, OpenOptions},
     io::{self},
     time::Duration,
 };
@@ -38,9 +40,38 @@ struct Args {
     file: Option<String>,
 }
 
+fn setup_log_path() -> Option<File> {
+    let log_path = dirs::home_dir()?
+        .join(".quill")
+        .join("logs")
+        .join("quill.log");
+
+    if let Err(err) = fs::create_dir_all(log_path.parent()?) {
+        eprintln!("quill: could not create log directory: {}", err);
+        return None;
+    }
+
+    match OpenOptions::new().create(true).append(true).open(&log_path) {
+        Ok(file) => {
+            eprintln!("quill: logging to {}", log_path.display());
+            Some(file)
+        }
+        Err(err) => {
+            eprintln!("quill: could not open log file: {}", err);
+            None
+        }
+    }
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
 
+    if let Some(log_file) = setup_log_path() {
+        env_logger::Builder::new()
+            .target(Target::Pipe(Box::new(log_file)))
+            .filter_level(log::LevelFilter::Debug)
+            .init();
+    }
     let _guard = TerminalGuard::enter()?;
     let size = Terminal::size()?;
 
@@ -53,6 +84,11 @@ fn main() -> Result<()> {
     } else {
         TextBuffer::empty()
     };
+    log::info!(
+        target: "buffer",
+        "CursorInfo:: col={}, row={} FileLen:: len={}",
+        buffer.col_idx(), buffer.row_idx(), buffer.rows().len()
+    );
     let status = StatusBar::from_buffer(&buffer, (buffer.col_idx(), buffer.row_idx()));
     let mut hl = Highlighting::default();
     hl.lang_changed(lang::Language::Rust);
@@ -60,6 +96,6 @@ fn main() -> Result<()> {
     renderer.set_info_msg("Text rendering complete, scrolling is left to be added.");
     renderer.render(&buffer, &mut hl, &status)?;
 
-    std::thread::sleep(Duration::from_secs(40));
+    std::thread::sleep(Duration::from_secs(20));
     Ok(())
 }
